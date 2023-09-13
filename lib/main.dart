9eace0190502cba29e8e3c0b11b2369f3293e790
rawisara <yodwaristanapak_r@silpakorn.edu>
2023-09-13 18:36:11 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:app_project/view/notidust_view.dart';
+import 'dart:async';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: NotiDustView(),
+    );
+  }
+}
---
+++ lib/view/notidust_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+class NotiDustView extends StatelessWidget {
+  const NotiDustView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
