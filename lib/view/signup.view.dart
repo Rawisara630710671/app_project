7076f1f0df79f2680f611c53bcd0cecf6631a995
rawisara <yodwaristanapak_r@silpakorn.edu>
2023-09-16 01:28:15 +0700

    .

--- lib/view/signup.view.dart
+++ lib/view/signup.view.dart
@@ -1,127 +1,175 @@
+import 'package:app_project/view/login.view.dart';
 import 'package:app_project/view/widgets/text.from.global.dart';
 import 'package:flutter/material.dart';
 import 'package:app_project/utils/global.colors.dart';
-import 'package:location/location.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class SignUpView extends StatelessWidget {
-  SignUpView({Key? key,
-    required this.currentLocation
+  const SignUpView({
+    Key? key,
   }) : super(key: key);
-  final TextEditingController userNameController = TextEditingController();
-  final TextEditingController phoneController = TextEditingController();
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController gpsController = TextEditingController();
-  final LocationData currentLocation;
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            size: 20,
+            color: GlobalColors.mainColor,
+          ),
+        ),
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+      ),
       body: SingleChildScrollView(
         child: SafeArea(
           child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            height: MediaQuery.of(context).size.height - 50,
             width: double.infinity,
-            padding: const EdgeInsets.all(15.0),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 20),
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text(
+                      'Noti DUST',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: GlobalColors.mainColor,
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Create New Account',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 15,),
+                Column(
+                  children: <Widget>[
+                    inputFile(label: "UserName"),
+                    inputFile(label: "Email"),
+                    inputFile(label: "Phone"),
+                    inputFile(
+                      label: "Password",
+                      obscureText: true,
+                    ),
+                    inputFile(
+                      label: "Comfirm Password",
+                      obscureText: true,
+                    ),
+                  ],
+                ),
                 Container(
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Noti DUST',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: GlobalColors.mainColor,
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
+                  padding: EdgeInsets.only(top: 3, left: 3),
+                  child: MaterialButton(
+                    minWidth: double.infinity,
+                    height: 30,
+                    onPressed: () {},
+                    color: GlobalColors.mainColor,
+                    elevation: 0,
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-                const SizedBox(height: 50,),
-                Text('Create New Account',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                  color: GlobalColors.textColor,
-                  fontSize: 25,
-                  fontWeight: FontWeight.w500,
+                const SizedBox(height: 40,),
+                Container(
+                  height: 50,
+                  color:  Colors.white,
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                          'Already have an account?'
+                      ),
+                      InkWell(
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            textStyle: const TextStyle(fontSize: 20),
+                          ),
+                          onPressed: (){
+                            Get.to(LoginView());
+                          },
+                          child: Text(
+                            ' Login',
+                            style: TextStyle(
+                              color: GlobalColors.mainColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 20,),
-                Text('User Name',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: GlobalColors.textColor,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                ///userName Input
-                const SizedBox(height: 10),
-                TextFormGlobal(
-                  controller: userNameController,
-                  text: 'your name',
-                  obscure: false,
-                  textInputType: TextInputType.name,
-                ),
-
-                const SizedBox(height: 20,),
-                Text('Phone',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: GlobalColors.textColor,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                ///Phone Input
-                const SizedBox(height: 10),
-                TextFormGlobal(
-                  controller: phoneController,
-                  text: 'your phone',
-                  obscure: false,
-                  textInputType: TextInputType.phone,
-                ),
-
-                const SizedBox(height: 20,),
-                Text('E-mail Address',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: GlobalColors.textColor,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                ///email Input
-                const SizedBox(height: 10),
-                TextFormGlobal(
-                  controller: emailController,
-                  text: 'your e-mail',
-                  obscure: false,
-                  textInputType: TextInputType.emailAddress,
-                ),
-
-                const SizedBox(height: 20,),
-                Text('Location',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: GlobalColors.textColor,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                ///GPS Input
-                const SizedBox(height: 10),
-                TextFormGlobal(
-                  controller: gpsController,
-                  text: 'your location',
-                  obscure: false,
-                  textInputType: TextInputType.name,
                 ),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget inputFile({label, obscureText = false}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            color: Colors.black,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        TextField(
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.grey.shade400,
+              ),
+            ),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Colors.grey.shade400,
+            )),
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+      ],
     );
   }
 }
